2d0c67b17075d39692e05765287efb69a14b8e80
OZida <nakuzida@gmail.com>
2018-03-18 21:59:51 +0200

    start

--- src/models/currencyModel.js
+++ src/models/currencyModel.js
@@ -1,7 +1,7 @@
 let store = {
 	data: [],
 	init: function(url){
-		return fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json').then((r)=>r.json()).then(json => store.data = json);
+		return fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11').then((r)=>r.json()).then(json => store.data = json);
 	}
 }
 export default store;
--- src/views/currencyEngine.js
+++ src/views/currencyEngine.js
@@ -1,20 +1,48 @@
 export default {
 	render: function(data) {
-		var body = document.getElementsByClassName('currency')[0];
+		var body_first = document.getElementsByClassName('currency')[0];
+		var body_second = document.getElementsByClassName('curr_radio')[0];
+
+		data.map(el => {
+			let label = document.createElement('label'),
+				input = document.createElement('input');
+
+			input.setAttribute('value', el.sale);
+			input.setAttribute('type', 'radio');
+			input.setAttribute('name', 'currency');
+			input.setAttribute('id', el.ccy);
+			label.innerHTML = el.ccy;
+
+			label.appendChild(input);
+			return label; 
+
+		}).forEach(function(el){
+				body_second.appendChild(el);
+		});
+		let usd = document.getElementById('USD');
+			usd.setAttribute('checked', 'checked');
+		let	btc = document.getElementById('BTC');
+		btc.value = btc.value * usd.value;
+
 
 		data.map(el => {
 			let div = document.createElement('div'),
-				h3 = document.createElement('h3'),
-				p = document.createElement('p');
-			h3.innerHTML = el.txt + ' (' +el.cc + ')';
-			p.innerHTML = el.rate;
-			div.appendChild(h3);
-			div.appendChild(p);
-			div.classList.add('currency_block');
-			return div;
-		}).forEach(function(el){
-				body.appendChild(el);
-		})
+				h3 = document.createElement('h4'),
+				p_buy = document.createElement('p'),
+				p_sale = document.createElement('p');
 
+			h3.innerHTML = el.ccy;
+			p_buy.innerHTML = 'Курс покупки - ' + el.buy;
+			p_sale.innerHTML = 'Курс продажи - ' + el.sale;
+
+			div.appendChild(h3);
+			div.appendChild(p_buy);
+			div.appendChild(p_sale);
+			div.classList.add('currency_block');
+			return div; 
+
+		}).forEach(function(el){
+				body_first.appendChild(el);
+		});
 	}
 }
